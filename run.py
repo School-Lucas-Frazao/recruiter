05992091b7e0cf18524cb70b91b30d58845b635b
School-Lucas-Frazao <116677214+School-Lucas-Frazao@users.noreply.github.com>
2024-03-05 10:17:37 -0600

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-# this is the file that will run the app
-
-from jobrecruiter import app
-
-if __name__ == '__main__': # This is a conditional statement so you can run the app
-    app.run(debug=True)
-
-
